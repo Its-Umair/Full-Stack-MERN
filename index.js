4baa2c55448c509e9c1950b807f856b2a59ea5ab
Umair Ali <itsumair.2021@gmail.com>
2024-04-16 19:52:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-        // 1 ways to print in javascript
-        alert("me")
-        document.write("this is a test")
-        // 2 javascript console api
-        console.log("Hello World");
-        console.warn("This is a test")
-        console.error("This is a error")
+// 1 ways to print in javascript
+alert("me")
+document.write("this is a test")
+// 2 javascript console api
+console.log("Hello World");
+console.warn("This is a test")
+console.error("This is a error")
