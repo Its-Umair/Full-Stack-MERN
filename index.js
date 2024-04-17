6c91ece8f8f769123f2c3e3da9544891fe69ded4
Umair Ali <itsumair.2021@gmail.com>
2024-04-17 19:57:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 //document.write("this is a test")
 
 // 2 javascript console api
-console.log("Hello World");
-console.warn("This is a test")
-console.error("This is a error")
+//console.log("Hello World");
+//console.warn("This is a test")
+//console.error("This is a error")
 
 // 3 javascript variable
 // containers to store data values
+var number1 = 34;
+var number2 = 59;
+console.log(number1 + number2);
