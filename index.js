f25d098e00e6a01273ed3329e41cd591e3362fe5
Umair Ali <itsumair.2021@gmail.com>
2024-05-05 20:19:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,10 @@ console.log("The value of a/b is ", a/b);
 // Assignment Operators
 var c = b;
 // c +=2;
-c -=2;
+// c -=2;
 // c *=2;
 // c /=2;
 console.log(c);
+var x= 34;
+var y = 56;
+console.log(x == y);
