2b45d39399b25c686f9be45da87d08656256d3c0
Umair Ali <itsumair.2021@gmail.com>
2024-05-05 20:08:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@
 // containers to store data values
 var number1 = 34;
 var number2 = 59;
-console.log(number1 + number2);
+// console.log(number1 + number2);
 
 // 4 data types in java script
 var num1 = "55";
 var str1 = "this is a string";
-console.log(num1 + str1);
+// console.log(num1 + str1);
 // objects
 
 var marks = {
@@ -24,19 +24,19 @@ var marks = {
     ali: 50,
     umair: 79
 }
-console.log(marks);
+// console.log(marks);
 
 // Boolean
 var a = true;
 var b = false;
-console.log(b);
+// console.log(b);
 
 // vr und;
 var und;
-console.log(und);
+// console.log(und);
 
 var n = null;
-console.log(n);
+// console.log(n);
 
 /*
 At a very high level, there are two types of data types in Javascript
@@ -45,4 +45,4 @@ At a very high level, there are two types of data types in Javascript
 */
 
 var arr = [1,2,3,4,5,6,7]
-console.log(arr)
+// console.log(arr)
