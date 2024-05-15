ad1a3fd46b110acc12c45f06cd893e0927e57dcf
Umair Ali <itsumair.2021@gmail.com>
2024-05-15 20:17:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -136,7 +136,8 @@ var arr = [1, 2, 3, 4, 5, 6, 7]
 // console.log(arr);
 for(var i = 0; i < arr.length;i++){
     if(i==2){
-        break
+        // break
+        continue;
     }
     console.log(arr[i]);
 }
